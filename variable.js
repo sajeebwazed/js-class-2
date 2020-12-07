b0db97248c3f6421a0823a15b1ef9e0c7909b9c5
Sajeeb Wazed <sojibwazedshuvo188@gmail.com>
2020-12-08 01:44:53 +0600

    added a new file variable.js

---
+++ variable.js
@@ -0,0 +1,8 @@
+var name ="sajeeb wazed"
+var age ="21"
+
+//
+
+consol.log('My name is sajeeb. I am from Bangladesh.')
+consol.log(name + 'know javascript')
+consol.log(name + 'is creating a javascript course for all')
