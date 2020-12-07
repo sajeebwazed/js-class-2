93013e77be31a7398bc56ed5f02eeb87fa19e0f9
Sajeeb Wazed <sojibwazedshuvo188@gmail.com>
2020-12-08 01:51:18 +0600

    edite this file &add new content

--- variable.js
+++ variable.js
@@ -6,3 +6,6 @@ var age ="21"
 consol.log('My name is sajeeb. I am from Bangladesh.')
 consol.log(name + 'know javascript')
 consol.log(name + 'is creating a javascript course for all')
+consol.log('But his age only' + age)
+consol.log('course free')
+consol.log(2500+2500)
